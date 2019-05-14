eb62ea7f1eb2b59f8b6779cd2db28a8ee565b83b
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-05-14 11:16:56 +0200

    fixed page naming

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
 
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import PageSidebar from '../components/page/PageSidebar';
 import BreadCrumb from '../components/BreadCrumb';
 import PageHero from '../components/PageHero';
