a6a77d9d4784eb4a802d539cff1d3d64b23edd41
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-05-14 12:09:59 +0200

    new try

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import Header from './Header';
+// import Header from './Header';
 import OverlayMenu from './OverlayMenu';
 import Hamburger from './Hamburger';
 import Footer from './Footer';
